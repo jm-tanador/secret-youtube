85335c4c491e0eae1e41bc5ba167eab5731d4bb5
jm-tanador <jmtanador7@gmail.com>
2026-08-03 11:21:20 +0800

    return

--- src/router/index.js
+++ src/router/index.js
@@ -24,42 +24,4 @@ const router = createRouter({
   ]
 });
 
-let cachedUserIp = null;
-
-async function getUserIp() {
-  if (cachedUserIp) return cachedUserIp;
-
-  try {
-    // Specify the full URL of your running backend server (e.g. Laravel port 8000)
-    const backendUrl = 'https://backend-production-5b9e.up.railway.app/api'; 
-    
-    const response = await fetch(backendUrl);
-    const data = await response.json();
-    cachedUserIp = data.ip; 
-    return cachedUserIp;
-  } catch (error) {
-    console.error("Failed to detect client IP:", error);
-    return null;
-  }
-}
-
-// Router Guard to check IP permissions before entering a route
-router.beforeEach(async (to, from, next) => {
-  // Always allow access to '/sorry' to prevent infinite redirection loops
-  if (to.path === '/sorry') {
-    return next();
-  }
-
-  const allowedIp = "10.169.142.172";
-  const userIp = await getUserIp();
-
-
-  if (userIp == allowedIp) {
-    next(); // IP matches, allow access to '/' or '/watch/:id'
-  } else {
-    console.log(userIp);
-    next({ name: 'sorry' }); // Redirect unauthorized users
-  }
-});
-
 export default router;
